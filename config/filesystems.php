2e1359c9411a1dbfd2d0e1e3173eb749b1fcbe68
freek <freek@spatie.be>
2020-09-03 14:49:20 +0200

    wip

--- config/filesystems.php
+++ config/filesystems.php
@@ -45,6 +45,7 @@ return [
         'media' => [
             'driver' => 'local',
             'root'   => public_path('media'),
+            'url' => env('APP_URL').'/media',
         ],
 
         'local' => [
